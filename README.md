09b38f15775d0425c80cd31b423d3dd8a2a07fcd
Will Ellwood <fragmad@gmail.com>
2017-02-21 22:06:29 +0000

    Improved formatting and added counters

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # GO File Stat Collect 
 
 A Go program to troll a file system for file stats and send it to a MongoDB document store. 
+
+## Benchmark 
+
+An example test on my home directory with print verbosity turned off. 
+
+```
+99250 items found.
+go run walk.go ~/  0.57s user 0.22s system 114% cpu 0.686 total
+```
+And with verbosity turned on. 
+
+```
+time go run walk.go ~/ 
+[majority of output omitted]
+99282 items found.
+go run walk.go ~/  1.81s user 2.30s system 69% cpu 5.957 total
+```
--- src/walk.go
+++ src/walk.go
@@ -2,27 +2,48 @@ package main
 
 import "path/filepath"
 import "os"
-
 import "io/ioutil"
+import "fmt"
+
+var count = 0
+var file_count = 0
+var directory_count = 0
+var verbose = false
+
+func print_file_stats(path string, info os.FileInfo) {
+	fmt.Println("--")
+	fmt.Print(path)
+	fmt.Println(info.Name())
+	fmt.Print(info.Size())
+	fmt.Println(" Bytes")
+	fmt.Println("Not a directory")
+}
+
+func print_directory_stats(path string, info os.FileInfo) {
+	fmt.Println("--")
+	fmt.Print(path)
+	fmt.Println(info.Name())
+	files, _ := ioutil.ReadDir(path)
+	fmt.Print(len(files))
+	fmt.Println(" files")
+	fmt.Println("A directory")
+}
 
 func walk_file_tree(dirPath string) {
 	filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
-			println("--")
-			print(path)
-			println(info.Name())
-			print(info.Size())
-			println(" Bytes")
-			println("Not a directory")
+			if verbose {
+				print_file_stats(path, info)
+			}
+			count++
+			file_count++
 		}
 		if info.IsDir() {
-			println("--")
-			print(path)
-			println(info.Name())
-			files, _ := ioutil.ReadDir(path)
-			print(len(files))
-			println(" files")
-			println("A directory")
+			if verbose {
+				print_directory_stats(path, info)
+			}
+			count++
+			directory_count++
 		}
 		return nil
 	})
@@ -32,4 +53,7 @@ func main() {
 	path := os.Args[1]
 
 	walk_file_tree(path)
+	fmt.Printf("%d items found.\n", count)
+	fmt.Printf("%d files found.\n", file_count)
+	fmt.Printf("%d directories found.\n", directory_count)
 }
