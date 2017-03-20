2f0478913f0327ac2090bd925698d8650357b8a3
Will Ellwood <fragmad@gmail.com>
2017-03-20 16:56:43 +0000

    Fixed bug caused by /proc
    walk.go can now search the whole hard disk and cope with "problems" caused by
    /proc on Linux systems.

--- src/walk.go
+++ src/walk.go
@@ -9,11 +9,30 @@ import "time"
 var count = 0
 var file_count = 0
 var directory_count = 0
-var verbose = true
+var verbose = false
 var start_time = time.Now()
 
 // Variable to store list of paths to ignore and not walk.
 // Ensure /proc is in this list.
+
+var ignored_paths = [1]string{"/proc"}
+
+// function to test if a file path is in the ignore list
+
+/* for key, value := range oldMap {
+    newMap[key] = value
+}*/
+
+func isPathinIgnoreList(path string) bool {
+	ignore_path := false
+	for _, test_path := range ignored_paths {
+		if path == test_path {
+			fmt.Println("ignoring", test_path)
+			ignore_path = true
+		}
+	}
+	return ignore_path
+}
 
 func print_file_stats(path string, info os.FileInfo) {
 	fmt.Println("--")
@@ -44,6 +63,14 @@ func print_final_stats() {
 func walk_file_tree(dirPath string) {
 	filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
 		// if path is in ignored paths don't continue
+		if isPathinIgnoreList(path) {
+			return nil
+		}
+
+		if err != nil {
+			return nil
+		}
+
 		if !info.IsDir() {
 			if verbose {
 				print_file_stats(path, info)
@@ -63,7 +90,11 @@ func walk_file_tree(dirPath string) {
 }
 
 func main() {
-	path := os.Args[1]
+	path := "."
+	if len(os.Args) > 1 {
+		path = os.Args[1]
+	}
+
 	walk_file_tree(path)
 	print_final_stats()
 }
